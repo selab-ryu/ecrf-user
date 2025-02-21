43d63a0f35b4ac37c1729f90a817b028f1698cda
selab-ryu <hkjkkl@yonsei.ac.kr>
2025-02-21 09:43:54 +0900

    [Side bar]
    - add Side Constants and Util

---
+++ ecrf-user-api/src/main/java/ecrf/user/constants/ECRFUserMenuConstants.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ecrf.user.constants;
+
+/**
+ * @author SELab-Ryu
+ *
+ */
+public class ECRFUserMenuConstants {
+	public static final String PROJECT_INFO = "project-info";
+    public static final String ADD_PROJECT = "add-project";
+    public static final String UPDATE_PROJECT = "update-project";
+    
+    public static final String EXP_GROUP = "exp-group";
+    public static final String ADD_EXP_GROUP = "add-exp-group";
+    public static final String UPDATE_GROUP = "update-exp-group";
+    public static final String LIST_EXP_GROUP = "list-exp-group";
+    
+    
+}
---
+++ ecrf-user-api/src/main/java/ecrf/user/constants/ECRFUserSidebarUtil.java
@@ -0,0 +1,12 @@
+package ecrf.user.constants;
+
+public class ECRFUserSidebarUtil {
+
+	
+	
+	public static String getSidebarClass(String menu, String target, boolean isActive) {
+		
+		
+		return "";
+	}
+}
