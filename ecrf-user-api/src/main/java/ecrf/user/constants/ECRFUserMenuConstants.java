f7363a2b499fdcdc948455abfdc43babb48377c2
dev-ryu <dev-ryu@DESKTOP-QBHLI2J>
2025-04-03 17:59:35 +0900

    [Side Navi]
    - imporve code & change design
      - refactoring code
      - change icon
      - change css

--- ecrf-user-api/src/main/java/ecrf/user/constants/ECRFUserMenuConstants.java
+++ ecrf-user-api/src/main/java/ecrf/user/constants/ECRFUserMenuConstants.java
@@ -8,14 +8,48 @@ package ecrf.user.constants;
  *
  */
 public class ECRFUserMenuConstants {
+	public static final String PROJECT = "project";
 	public static final String PROJECT_INFO = "project-info";
     public static final String ADD_PROJECT = "add-project";
     public static final String UPDATE_PROJECT = "update-project";
     
     public static final String EXP_GROUP = "exp-group";
-    public static final String ADD_EXP_GROUP = "add-exp-group";
-    public static final String UPDATE_GROUP = "update-exp-group";
     public static final String LIST_EXP_GROUP = "list-exp-group";
+    public static final String ADD_EXP_GROUP = "add-exp-group";
+    public static final String UPDATE_EXP_GROUP = "update-exp-group";
     
+    public static final String RESEARCHER = "researcher";
+    public static final String LIST_RESEARCHER = "list-researcher";
+    public static final String ADD_RESEARCHER = "add-researcher";
+    public static final String UPDATE_RESEARCHER = "update-researcher";
+    public static final String VIEW_RESEARCHER = "view-researcher";
     
+    public static final String SUBJECT = "subject";
+    public static final String LIST_SUBJECT = "list-subject";
+    public static final String ADD_SUBJECT = "add-subject";
+    public static final String UPDATE_SUBJECT = "update-researcher";
+    public static final String VIEW_SUBJECT = "view-researcher";
+    
+    public static final String CRF = "crf";
+    public static final String LIST_CRF = "list-crf";
+    public static final String ADD_CRF = "add-crf";
+    public static final String UPDATE_CRF = "update-crf";
+    
+    public static final String CRF_FORM = "crf-form";
+    public static final String MANAGE_FORM = "manage-form";
+    public static final String PDF_DOWNALOD = "pdf-download";
+    
+    public static final String CRF_DATA = "crf-data";
+    public static final String LIST_CRF_DATA = "list-crf-data";
+    public static final String VIEW_CRF_DATA = "view-crf-data";
+    public static final String ADD_CRF_DATA = "add-crf-data";
+    public static final String UPDATE_CRF_DATA = "update-crf-data";
+    public static final String LIST_HISTORY = "list-history";
+    public static final String EXCEL_DOWNLOAD = "excel-download";
+    public static final String SEARCH_CRF_DATA = "search-crf-data";
+    
+    public static final String CRF_QUERY = "crf-query";
+    public static final String LIST_QUERY = "list-query";
+    public static final String ADD_QUERY = "add-query";
+    public static final String VALIDATE_QUERY = "validate-query";
 }
